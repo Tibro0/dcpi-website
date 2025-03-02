98a419e7a444e181152643681de4a93b660080dd
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-03-02 13:00:29 +0600

    Frontend Mastering

---
+++ resources/views/frontend/layouts/master.blade.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <!-- mobile responsive meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- ** Plugins Needed for the Project ** -->
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('frontend/plugins/bootstrap/bootstrap.min.css') }}">
+    <!-- slick slider -->
+    <link rel="stylesheet" href="{{ asset('frontend/plugins/slick/slick.css') }}">
+    <!-- themefy-icon -->
+    <link rel="stylesheet" href="{{ asset('frontend/plugins/themify-icons/themify-icons.css') }}">
+    <!-- animation css -->
+    <link rel="stylesheet" href="{{ asset('frontend/plugins/animate/animate.css') }}">
+    <!-- aos -->
+    <link rel="stylesheet" href="{{ asset('frontend/plugins/aos/aos.css') }}">
+    <!-- venobox popup -->
+    <link rel="stylesheet" href="{{ asset('frontend/plugins/venobox/venobox.css') }}">
+    <!-- Main Stylesheet -->
+    <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet">
+    <!--Favicon-->
+    <link rel="shortcut icon" href="{{ asset('frontend/images/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('frontend/images/favicon.ico') }}" type="image/x-icon">
+    @stack('frontend-css')
+</head>
+
+<body>
+    <!-- header -->
+    @include('frontend.layouts.header')
+    <!-- /header -->
+    <!-- Register Modal start -->
+    {{-- <div class="modal fade" id="signupModal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content rounded-0 border-0 p-4">
+                <div class="modal-header border-0">
+                    <h3>Register</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="login">
+                        <form action="#" class="row">
+                            <div class="col-12">
+                                <input type="text" class="form-control mb-3" id="signupPhone" name="signupPhone"
+                                    placeholder="Phone">
+                            </div>
+                            <div class="col-12">
+                                <input type="text" class="form-control mb-3" id="signupName" name="signupName"
+                                    placeholder="Name">
+                            </div>
+                            <div class="col-12">
+                                <input type="email" class="form-control mb-3" id="signupEmail" name="signupEmail"
+                                    placeholder="Email">
+                            </div>
+                            <div class="col-12">
+                                <input type="password" class="form-control mb-3" id="signupPassword"
+                                    name="signupPassword" placeholder="Password">
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary">SIGN UP</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+    <!-- Register Modal end -->
+    <!-- Login Modal Start -->
+    {{-- <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content rounded-0 border-0 p-4">
+                <div class="modal-header border-0">
+                    <h3>Login</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="#" class="row">
+                        <div class="col-12">
+                            <input type="text" class="form-control mb-3" id="loginPhone" name="loginPhone"
+                                placeholder="Phone">
+                        </div>
+                        <div class="col-12">
+                            <input type="text" class="form-control mb-3" id="loginName" name="loginName"
+                                placeholder="Name">
+                        </div>
+                        <div class="col-12">
+                            <input type="password" class="form-control mb-3" id="loginPassword" name="loginPassword"
+                                placeholder="Password">
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary">LOGIN</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+    <!-- Login Modal End -->
+
+    @yield('frontend-content')
+
+    <!-- footer Start -->
+    @include('frontend.layouts.footer')
+    <!-- footer End -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('frontend/plugins/jQuery/jquery.min.js') }}"></script>
+    <!-- Bootstrap JS -->
+    <script src="{{ asset('frontend/plugins/bootstrap/bootstrap.min.js') }}"></script>
+    <!-- slick slider -->
+    <script src="{{ asset('frontend/plugins/slick/slick.min.js') }}"></script>
+    <!-- aos -->
+    <script src="{{ asset('frontend/plugins/aos/aos.js') }}"></script>
+    <!-- venobox popup -->
+    <script src="{{ asset('frontend/plugins/venobox/venobox.min.js') }}"></script>
+    <!-- mixitup filter -->
+    <script src="{{ asset('frontend/plugins/mixitup/mixitup.min.js') }}"></script>
+    <!-- google map -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU&libraries=places">
+    </script>
+    <script src="{{ asset('frontend/plugins/google-map/gmap.js') }}"></script>
+    <!-- Main Script -->
+    <script src="{{ asset('frontend/js/script.js') }}"></script>
+    @stack('frontend-js')
+</body>
+
+</html>
